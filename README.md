8c5407500b0a120f41de5a774ca05de4f0526b8d
Temp User <philipwalshpw@gmail.com>
2026-02-03 15:32:42 +0000

    add demo files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# thoughtful-assistant-demo
-magine you are working at Thoughtful AI. The team has tasked you with building a simple customer support AI Agent to assist users with basic questions about Thoughtful AI.
+---
+title: Thoughtful Assist Demo
+emoji: 💡
+colorFrom: indigo
+colorTo: green
+sdk: gradio
+sdk_version: 5.34.2
+app_file: app.py
+pinned: false
+short_description: A simple customer support AI agent for Thoughtful AI
+---
+
+# Thoughtful Assist Demo
+
+Thoughtful Assist demo was designed to for the [Thoughtful AI Technical Screening](https://thoughtfulautomation.notion.site/AI-Technical-Screen-d4d381a8c38d40fc9287cdb6c4f9992a) it's a minimal customer support agent that answers common questions about Thoughtful AI using semantic retrieval, with an LLM fallback for unmatched queries (Fallback uses DistilGPT2, a generic text generator, so when no Q&A match is found it may produce off-topic or nonsensical responses).
+
+View it on Hugging Face
+**(DEMO)[todo]**
+
+---
+
+## Steps to run locally
+
+```sh
+# Clone the repository
+git clone https://github.com/Philip-Walsh/thoughtful-assistant-demo
+cd thoughtful-assist
+
+# Create and activate the env
+python -m venv venv
+source venv/bin/activate
+
+# Install dependencies:
+pip install -r requirements.txt
+
+
+# Run it
+python app.py
+
+```
---
+++ app.py
@@ -0,0 +1,59 @@
+from sentence_transformers import SentenceTransformer
+from transformers import pipeline
+import numpy as np
+import gradio as gr
+from qa import QA
+
+
+class ChatBot:
+    def __init__(self, threshold: float = 0.6):
+        self.threshold = threshold
+        self.model = None
+        self.llm = None
+        self.questions = []
+        self.answers = []
+        self.q_embeddings = None
+
+        self._load_qa(QA)
+        self._set_embedding_model("all-MiniLM-L6-v2")
+        self._set_fallback_model("distilgpt2")
+
+
+    def _set_embedding_model(self, model_name: str):
+        self.model = SentenceTransformer(model_name)
+        self.q_embeddings = self.model.encode(self.questions)
+
+    def _set_fallback_model(self, model_name: str):
+        self.llm = pipeline("text-generation", model=model_name)
+
+    def _load_qa(self, qa_data: dict):
+        for item in qa_data.get("questions", []):
+            self.questions.append(item["question"])
+            self.answers.append(item["answer"])
+
+
+    def retrieve(self, query: str):
+        query_emb = self.model.encode([query])[0]
+        sims = np.dot(self._q_embeddings, query_emb)
+        best_idx = np.argmax(sims)
+        return sims[best_idx], self.answers[best_idx]
+
+    def fallback(self, query: str):
+        result = self.llm(query, max_new_tokens=80)[0]["generated_text"]
+        return result
+
+    def chat(self, user_input: str, history=None):
+        score, answer = self.retrieve(user_input)
+        if score >= self.threshold:
+            return answer
+        return self.fallback(user_input)
+
+
+def main():
+    bot = ChatBot()
+    ui = gr.ChatInterface(bot.chat)
+    ui.launch()
+
+
+if __name__ == "__main__":
+    main()
